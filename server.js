233184726ab6f2f4fe2d282bb125384c51227873
Nikhilioten10 <nikhilchanda2210tech@gmail.com>
2016-09-23 11:23:22 +0530

    server code is also updated

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/maxresdefault.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'maxresdefault.jpg'));
+app.get('/ui/pp.jpe', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'pp.jpe'));
 });
 
 
