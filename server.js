69e54d40d66e468d0aa1577b5e46074a5f556373
Nikhilioten10 <nikhilchanda2210tech@gmail.com>
2016-09-23 09:06:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/maxresdefault.jpg', function (req, res) {
+app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'maxresdefaul.jpg'));
 });
 
