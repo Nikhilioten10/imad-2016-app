d8ae1132378bd652a05450212ae124dd8b94757f
Nikhilioten10 <nikhilchanda2210tech@gmail.com>
2016-09-24 13:23:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,59 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var article1 = {
+    title: 'Article One',
+    heading:'ARTICLE ONE', 
+    date: 'DATE : september 23, 2016.',
+    content: 
+    `<p>
+	Fire in the heart.Fire in the heart.Fire in the heart.Fire in the heart.Fire in the heart.Fire in the heart.Fire in the heart.
+	Fire in the heart.Fire in the heart.Fire in the heart.Fire in the heart.Fire in the heart.Fire in the heart.Fire in the heart.
+	Fire in the heart.Fire in the heart.Fire in the heart.Fire in the heart.Fire in the heart.Fire in the heart.Fire in the heart.
+	</p>
+	<p>
+	Fire in the heart.Fire in the heart.Fire in the heart.Fire in the heart.Fire in the heart.Fire in the heart.Fire in the heart.
+	Fire in the heart.Fire in the heart.Fire in the heart.Fire in the heart.Fire in the heart.Fire in the heart.Fire in the heart.
+	Fire in the heart.Fire in the heart.Fire in the heart.Fire in the heart.Fire in the heart.Fire in the heart.Fire in the heart.
+	</p>
+	<p>
+	Fire in the heart.Fire in the heart.Fire in the heart.Fire in the heart.Fire in the heart.Fire in the heart.Fire in the heart.
+	Fire in the heart.Fire in the heart.Fire in the heart.Fire in the heart.Fire in the heart.Fire in the heart.Fire in the heart.
+	Fire in the heart.Fire in the heart.Fire in the heart.Fire in the heart.Fire in the heart.Fire in the heart.Fire in the heart.
+	</p>`
+};
+function createTemplate(data)
+{
+var title = data.title;
+var heading = data.heading;
+var date = data.date;
+var cintent = data.cintent;
+var htmlTemplate = `<html>
+<head>
+    <title>${title}</title>
+	   <meta name="viewport" content="width-device-width,initial-scale=1">
+	   <link href="/ui/articleStyle.css" rel="stylesheet" />
+</head>
+<body>
+	<a  href="/">HOME</a>
+	<hr>
+ <div id = "main-content" class = "container">
+	<div>
+	  <h3>
+	  ${heading}</h3>
+	</div>
+	<div id = "date">
+	   ${date}
+	</div>
+	<div>
+	${content}
+	</div>
+</div>
+</body>
+</html>`;
+
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -12,6 +65,10 @@ app.get('/', function (req, res) {
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/articleStyle.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'articleStyle.css'));
 });
 
 app.get('/ui/article1.html', function (req, res) {
