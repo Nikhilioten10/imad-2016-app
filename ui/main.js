caf50472d795b258b91f6a67ab213363ed47825e
Nikhilioten10 <nikhilchanda2210tech@gmail.com>
2016-09-25 20:46:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ var img = document.getElementById('img');
 var marginLeft = 0;
 function moveRight(){
     marginLeft = marginLeft + 10;
-    img.style.marginLeft = marginLeft;
+    img.style.marginLeft = marginLeft+'px';
 }
 img.onclick = function()
 {
