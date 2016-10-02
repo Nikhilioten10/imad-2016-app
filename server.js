0e7801bbee9412e18f9467d99e37173063351ebe
Nikhilioten10 <nikhilchanda2210tech@gmail.com>
2016-10-02 11:05:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,6 +78,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/n.ico', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'n,ico'));
+});
+
 app.get('/ui/articleStyle.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'articleStyle.css'));
 });
