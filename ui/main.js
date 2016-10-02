0872c99e03b84dc355a29750253e7e2fcc34e855
Nikhilioten10 <nikhilchanda2210tech@gmail.com>
2016-10-02 11:15:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,9 @@
 console.log('Loaded!');
 
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick =  function(){
+    counter = counter+1;
+ var span = document.getElementbyId('count');
+ span.innerHTML = span;
+};
