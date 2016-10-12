a27265228dc3221daedefd69e48a8098423a783d
Nikhilioten10 <nikhilchanda2210tech@gmail.com>
2016-10-12 16:09:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -76,3 +76,37 @@ button3.onclick = function(){
     
 };
 
+
+var sub_button = document.getElementById('submit_btn');
+sub_button.onclick = function(){
+    
+    var request = new XMLHttprequest();
+    
+    request.onreadystatechange = function(){
+        
+        if(request.readyState == XMLHttpRequest.DONE)
+        {
+            if(request.status == 200)
+            {
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                
+                for(var i=0;i<names.length;i++)
+                {
+                    list+= '<li>'+names[i]+'<li>'
+                }
+                
+                var ul = document.getElementById('nameslist');
+                ul.innerHtml = list;
+            
+            }
+        }
+        
+    };
+    
+    var nameInput = document.getElementById('comments');
+    var name = nameInput.value;
+    request.open('GET',"http://nikhilioten10.imad.hasura-app.io/submit-name?name="+name,true);
+    request.send(null);
+};
