43f4b065539db9ce2a63f168c802f1ffda8ea42d
Nikhilioten10 <nikhilchanda2210tech@gmail.com>
2016-10-14 20:35:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,12 +90,12 @@ app.get('/ui/bBMGRPFn.jpg', function (req, res) {
   res.sendFile(path.join(__dirname,'ui', 'bBMGRPFn.jpg'));
 });
 
-app.get('/ui/pp3.JPG', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'pp3.JPG'));
+app.get('/ui/pp5.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'pp3.jpg'));
 });
 
-app.get('/ui/pp4.JPG', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'pp4.JPG'));
+app.get('/ui/pp6.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'pp6.jpg'));
 });
 
 app.get('/ui/pp.jpe', function (req, res) {
