c1c275e4f975dc7274a6f50de871b000d96b81a1
Nikhilioten10 <nikhilchanda2210tech@gmail.com>
2016-10-14 10:28:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,6 +91,7 @@ app.get('/submit-name', function (req, res) { //url: submit-name?name=xxxx
 
 
 app.get('/:urlName', function (req, res) {
+    var urlName = req.params.urlName;
   res.send(createTemplate(Urls[urlName]));
 });
 
