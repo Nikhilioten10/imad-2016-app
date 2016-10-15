cf7bed9ee78c8e50cd0dca34fb9cee29cb36bea5
Nikhilioten10 <nikhilchanda2210tech@gmail.com>
2016-10-15 14:45:20 +1100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,10 +20,10 @@ var Urls = {
 	   <ul>
 	    <li>Big Data Analytics</li>
 	    <li>Internet of Things</li>
-	    <li>MEAN Stack we development</li>
+	    <li>MEAN Stack web development</li>
 	    <li>Complete java</li>
 	    <li>Oracle database management</li>
-	    <li>Data Sceince<li>
+	    <li>Data Sceince</li>
 	   </ul>
    </p>
    <p>
@@ -37,7 +37,7 @@ var Urls = {
     date: 'DATE : October 15, 2016.',
     content: 
     `<p>
-      <h4>Tehcnical SKILLS</h4>
+      <h4>Tehcnical Skills</h4>
 	   <ui>
 	   <li>C programming</li>
 	   <li>Core Java</li>
@@ -47,12 +47,12 @@ var Urls = {
 	   <li>Python</li>
 	   </ul>
 	   
-	   <h4>Professional Skiils</h4>
+	   <h4>Professional Skills</h4>
 	   <ul>
 	   <li>Being able to Communicate Well.</li>
 	   <li>Being able to Volunteer for Organisations likw IEEE, CSI.</li>
 	   <li>Being adaptive in nature.</li>
-	   <li>Being able to mingle with new people as quivkly.</li>
+	   <li>Being able to mingle with new people as quickly.</li>
 	   <li>I am a Learner, Motivator, Orator, Dominator and a Special entity in my working environment.</li>
 	   </ul>
 	</p>`
@@ -68,9 +68,9 @@ var Urls = {
    organisation: EXPERSHUB , Industrial skill development center.<br>
    venue: JSS Academy of Technical Education.<br>
    Place : Uttarahali,BANGALORE.<br>
-   State: Karnataka.<br>
+   State: Karnataka.<br><br>
   <article id="Experience">
-   My experience: <p>This Internship have lime lighted my self how to grow as a leader, learner and motivator in my professioanl career. This was a virtual internship conducted in an engineering college inheriting the industry environment to be able to understand requirements of IT industries.
+   <h4>My experience</h4>: <p>This Internship have lime lighted my self how to grow as a leader, learner and motivator in my professioanl career. This was a virtual internship conducted in an engineering college inheriting the industry environment to be able to understand requirements of IT industries.
      </p>
      <p>
         Here we were divited into twenty teams.My team name is 'IOTENS'.I was a team leader and a software engineer for
@@ -93,7 +93,7 @@ var Urls = {
     </ul><br>
     Mobile: 8374249824, 8374079824.<br>
     Facebook : https://www.facebook.com/nikhil.chandha<br>
-    linkedin: Public Profilehttps://in.linkedin.com/in/nikhil-chanda-656569107<br>
+    linkedin: https://in.linkedin.com/in/nikhil-chanda-656569107<br>
     Git Hub: https://github.com/Nikhilioten10<br>
     </p>`
 }
