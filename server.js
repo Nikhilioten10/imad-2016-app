a97e52564eb978f9eda6c0a58539230395e29345
Nikhilioten10 <nikhilchanda2210tech@gmail.com>
2016-12-25 19:40:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -139,7 +139,7 @@ app.get('/', function (req, res) {
 });
 
 var pool = new Pool(config);
-app.get('/test-db',function(req,res){
+app.get('/db',function(req,res){
     
     pool.query('SELECT * FROM test',function(err,result){
         
