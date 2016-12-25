a48da57bf603e9cd19cc690b268291f0d5d8d328
Nikhilioten10 <nikhilchanda2210tech@gmail.com>
2016-12-25 19:59:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -165,9 +165,9 @@ app.get('/db',function(req,res){
     pool.query('SELECT * FROM test',function(err,result){
         
         if(err){
-            res.status(500).send(err.toSring());
+            res.status(500).send(err.toString());
         }else{
-            res.send(JSON.Stringfy(result));
+            res.send(JSON.stringify(result));
         }
     });
 });
