9f5e053233f7a6366bd0b7bdc96fa02b015a4d96
Nikhilioten10 <nikhilchanda2210tech@gmail.com>
2016-12-25 20:39:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -144,18 +144,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-var pool = new Pool(config);
-app.get('/test-db',function(req,res){
-    
-    pool.query("SELECT * FROM 'test'",function(err,result){
-        
-        if(err){
-            res.status(500).send(err.toString());
-        }else{
-            res.send(JSON.stringify(result));
-        }
-    });
-});
+
+app.get('/test-db',);
 
 
 var counter = 0;
@@ -176,7 +166,24 @@ app.get('/submit-name', function (req, res) { //url: submit-name?name=xxxx
 
 app.get('/:urlName', function (req, res) {
     var urlName = req.params.urlName;
-  res.send(createTemplate(Urls[urlName]));
+    var pool = new Pool(config);
+    if(urlName == 'test-db',function(req,res){
+    
+    pool.query("SELECT * FROM 'test'",function(err,result){
+        
+        if(err){
+            res.status(500).send(err.toString());
+        }else{
+            res.send(JSON.stringify(result));
+        }
+    });
+
+        
+    }){}
+    else{
+    
+    res.send(createTemplate(Urls[urlName]));
+    }
 });
 
 
